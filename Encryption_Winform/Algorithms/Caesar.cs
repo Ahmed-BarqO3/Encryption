f145ef63571aecc108c914047f4b81d1f0ec5e8a
AHMED <ahmed.barg03@gmail.com>
2024-05-04 21:22:46 +0200

    intial commit

---
+++ Encryption_Winform/Algorithms/Caesar.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption_Winform.Algorithms
+{
+    public abstract class Caesar
+    {
+
+        public static string Encryption(string P,int K)
+        {
+           var sympole = new char[]  { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+            StringBuilder C = new StringBuilder();
+
+            for (int i = 0; i < P.Length; i++)
+            {
+                for (int j = 0;j < 26;j++)
+                if (P[i] == sympole[j])
+                    if ( j + K > 25)
+                    {
+                        C.Append(sympole[Math.Abs(25 - (j + K) - 1)]);
+                    }
+                    else
+                        C.Append(sympole[j + K]);
+                
+            }
+            return C.ToString();
+        }
+
+        public static string Decryption(string C, int K)
+        {
+            var sympole = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+            StringBuilder P = new StringBuilder();
+
+            for (int i = 0; i < C.Length; i++)
+            {
+                for (int j = 0; j < 26; j++)
+                    if (C[i] == sympole[j])
+                    {
+                        if (j - K < 0)
+                        {
+                            P.Append(sympole[(j - K) + 26]);
+                        }
+                        else
+                            P.Append(sympole[j - K]);
+                    }
+            }
+            return P.ToString();
+        }
+
+    }
+}
